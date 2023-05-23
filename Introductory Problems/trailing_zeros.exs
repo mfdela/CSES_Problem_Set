ec8f50e3ce8a2e1bf59c82cbe4b66f21099e40d2
Marco Delaurenti <mfd@fastmail.com>
2023-05-23 16:00:07 +0100

    More idiomatic elixir

--- Introductory Problems/trailing_zeros.exs
+++ Introductory Problems/trailing_zeros.exs
@@ -28,18 +28,13 @@ defmodule TrailingZeros do
   # to get a zero we need to have a power of 2 and a power of 5 in the factorial
   # each 2 * 5 gives a 0. Since the exponent of 5 is always < than the exponent
   # of 2, we just need to count the powers of 5 in the factorial.
-  defp trailing_zeros(n), do: power_5(n, 1, 0)
+  defp trailing_zeros(n), do: power_5(n, 1, div(n, 5), 0)
 
-  defp power_5(n, exp, count) do
-    f = div(n, 5 ** exp)
-    cond do
-      f == 0 -> count
-      true -> power_5(n, exp + 1, count + f)
-    end
-  end
+  defp power_5(_n, _exp, 0, count), do: count
+  defp power_5(n, exp, f, count), do: power_5(n, exp + 1,  div(n, 5 ** (exp + 1)), count + f)
 
   def run(), do: read_file("input.txt") |> trailing_zeros() |> IO.puts
-  
+
 end
 
 TrailingZeros.run()
